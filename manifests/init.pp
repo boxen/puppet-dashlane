10aa2d512e630ec213f397895f699dffcab8fa9a
Pongstr <twiz.tickler@gmail.com>
2015-07-14 18:06:17 +0800

    Upgrade Dashlane to v3.5.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class dashlane {
   package { 'dashlane':
     provider => 'appdmg',
-    source   => 'https://d3mfqat9ni8wb5.cloudfront.net/releases/2.1.1/2.1.1.38498/Dashlane.dmg'
+    source   => 'https://d3mfqat9ni8wb5.cloudfront.net/releases/3.5.2/3.5.2.88962/Dashlane.dmg'
   }
 }
--- spec/classes/dashlane_spec.rb
+++ spec/classes/dashlane_spec.rb
@@ -9,7 +9,7 @@ describe 'dashlane' do
 
   it do
     should contain_package('dashlane').with({
-      :source   => 'https://d3mfqat9ni8wb5.cloudfront.net/releases/2.1.1/2.1.1.38498/Dashlane.dmg',
+      :source   => 'https://d3mfqat9ni8wb5.cloudfront.net/releases/3.5.2/3.5.2.88962/Dashlane.dmg',
       :provider => 'appdmg'
     })
   end
